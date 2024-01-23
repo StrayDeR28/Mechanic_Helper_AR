2fee8fbc53fa77d3515f90a4e5bbdf06dfed4d47
Фёдор <fedor.pavlov.02@mail.ru>
2024-01-23 18:36:04 +0700

    Tutorial done (need to refactor)

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Element : MonoBehaviour
+{
+    [SerializeField] internal string Instruction { get; set; }
+    [SerializeField] internal string Heading { get; set; }
+
+}
--- Assets/Scripts/TutorialSteps.cs
+++ Assets/Scripts/TutorialSteps.cs
@@ -6,7 +6,8 @@ using TMPro;
 public class TutorialSteps : MonoBehaviour
 {
     [SerializeField] private List<GameObject> elements = new List<GameObject>();
-    [SerializeField] private List<string> instructions = new List<string>();
+    [SerializeField] private List<string> instructionsAssembly = new List<string>();
+    [SerializeField] private List<string> instructionsDisassembly = new List<string>();
     [SerializeField] private List<string> headings = new List<string>();
     [SerializeField] private bool isAssembling = true;//сборка/разборка
 
@@ -14,10 +15,13 @@ public class TutorialSteps : MonoBehaviour
     [SerializeField] private GameObject instruction;
 
     private int i = 0;
+    [SerializeField] private string congrats = "ura pobeda";
+
     public void SetAssemblingType(bool flag)
     {
         isAssembling = flag;
-        if (isAssembling) { SetAssembling(); }
+        SetElementParametrs(isAssembling);
+        if (isAssembling) { SetAssembling();}
         else { SetDisassembling(); }
     }
     public void SetAssembling()
@@ -27,8 +31,8 @@ public class TutorialSteps : MonoBehaviour
         {
             elements[j].SetActive(false);
         }
-        heading.GetComponent<TMP_Text>().text = headings[i];
-        instruction.GetComponent<TMP_Text>().text = instructions[i];
+        heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
+        instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
     }
     public void SetDisassembling()
     {
@@ -37,18 +41,79 @@ public class TutorialSteps : MonoBehaviour
         {
             elements[j].SetActive(true);
         }
-        heading.GetComponent<TMP_Text>().text = headings[i];
-        instruction.GetComponent<TMP_Text>().text = instructions[i];
+        heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
+        instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
+    }
+    public void SetElementParametrs(bool isAssemble)
+    {
+        for(int j = 0;j < elements.Count;j++)
+        {
+            elements[j].GetComponent<Element>().Instruction = isAssemble ? instructionsAssembly[j] : instructionsDisassembly[j];
+            elements[j].GetComponent<Element>().Heading = headings[j];
+        }
     }
     public void NextStep()
     {
         if(isAssembling)
-        {
-
+        { 
+            if(i<=elements.Count -1)
+                elements[i].SetActive(true);
+            if (i == elements.Count - 1)
+            {
+                i++;
+                heading.GetComponent<TMP_Text>().text = congrats;
+                instruction.GetComponent<TMP_Text>().text = " ";
+            }
+            if (i < elements.Count - 1)
+            {
+                i++;
+                heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
+                instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
+            }
+           
+        }
+        else
+        { 
+            if(i>=0)
+                elements[i].SetActive(false);
+            if (i == 0)
+            {
+                i--;
+                heading.GetComponent<TMP_Text>().text = congrats;
+                instruction.GetComponent<TMP_Text>().text = " ";
+            }
+            if (i > 0 && elements[i - 1] != null)
+            {
+                i--;
+                heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
+                instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
+            }
+           
         }
     }
     public void PrevStep() 
-    { 
-
+    {
+        if (isAssembling)
+        {
+            
+            if (i > 0 && elements[i - 1] != null)
+            {
+                i--;
+                elements[i].SetActive(false);
+                heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
+                instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
+            }
+        }
+        else
+        {
+            
+            if (i < elements.Count - 1)
+            {
+                i++;
+                elements[i].SetActive(true);
+                heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
+                instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
+            }
+        }
     }
 }
