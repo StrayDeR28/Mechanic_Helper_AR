48b0efd4482e8001bdd6f519295df0df8f367be8
Фёдор <fedpr.pavlov.02@mail.ru>
2024-01-24 21:47:20 +0700

    Refactored tutorial script
    Переписал скрипт туториала, а именно часть перекрашивающую элементы в зеленый. Теперь функционал перекраса лежит в классе Element

--- Assets/Scripts/Element.cs
+++ Assets/Scripts/Element.cs
@@ -8,4 +8,62 @@ public class Element : MonoBehaviour
     [SerializeField] internal string Instruction { get; set; }
     [SerializeField] internal string Heading { get; set; }
 
+    [SerializeField] private List<Color> defaultColors = new List<Color>();
+    [SerializeField] private List<Transform> parts = new List<Transform>();
+
+    private void Awake()
+    {
+        int childCount = 0;//количество детей без детей + количество детей у детей
+        for (int k = 0; k < gameObject.transform.childCount; k++)//считаем количество элементов с материалами
+        {
+            Transform child = gameObject.transform.GetChild(k);
+            if (child.transform.childCount > 0)
+            {
+                for (int j = 0; j < child.transform.childCount; j++)
+                {
+                    childCount++;
+                }
+            }
+            else { childCount++; }
+        }
+        int childIterator = 0;
+        for (int i = 0; i < childCount; i++)//Заносим в лист цветов изначальные цвета
+        {
+            Transform child = gameObject.transform.GetChild(childIterator);
+            if (child.GetComponent<MeshRenderer>())
+            {
+                defaultColors.Add(child.GetComponent<MeshRenderer>().material.color);
+                parts.Add(child);
+            }
+            if (child.transform.childCount > 0)//Если у ребенка есть дети
+            {
+                for(int j = 0; j < child.transform.childCount; j++)
+                {
+                    Transform child2 = child.transform.GetChild(j);
+                    if (child2.GetComponent<MeshRenderer>())
+                    {
+                        defaultColors.Add(child2.GetComponent<MeshRenderer>().material.color);
+                        parts.Add(child2);
+                    }
+                }
+            }
+            childIterator++;
+        }
+    }
+
+    public void SetToDefaultColor()
+    {
+        for ( int i = 0; i < defaultColors.Count; i++)
+        {
+            parts[i].GetComponent<MeshRenderer>().material.color = defaultColors[i];
+        }
+    }
+    public void SetToGreenColor()
+    {
+        for (int i = 0; i < defaultColors.Count; i++)
+        {
+            parts[i].GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0, 0.5f);
+            //для анимаций сюда добавить флаг -> стал зеленым = запускаем анимацию. Или отслеживать подругому, так пока проще, для демки норм будет решение
+        }
+    }
 }
--- Assets/Scripts/TutorialSteps.cs
+++ Assets/Scripts/TutorialSteps.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Xml.Linq;
 
 public class TutorialSteps : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class TutorialSteps : MonoBehaviour
     [SerializeField] private GameObject instruction;
 
     private int i = 0;
-    private Color tmpColor = Color.white;
+    //private Color tmpColor = Color.white;
     [SerializeField] private string congrats = "ura pobeda";
 
     public void SetAssemblingType(bool flag)
@@ -27,7 +28,7 @@ public class TutorialSteps : MonoBehaviour
     }
     public void SetAssembling()
     {
-        if (i >= 0 && i <= 14) elements[i].GetComponentInChildren<MeshRenderer>().material.color = tmpColor;
+        if (i >= 0 && i < elements.Count) elements[i].GetComponent<Element>().SetToDefaultColor();
 
         i = 0;
         for(int j = 0; j < elements.Count; j++) 
@@ -35,8 +36,7 @@ public class TutorialSteps : MonoBehaviour
             elements[j].SetActive(false);
         }
         //Задаем следующий элемент зеленым
-        tmpColor = elements[i].GetComponentInChildren<MeshRenderer>().material.color;
-        elements[i].GetComponentInChildren<MeshRenderer>().material.color = new Color(0,1,0,0.5f);
+        elements[i].GetComponent<Element>().SetToGreenColor();
         elements[i].SetActive(true);
 
         heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
@@ -44,7 +44,7 @@ public class TutorialSteps : MonoBehaviour
     }
     public void SetDisassembling()
     {
-        if(i>=0 && i<=14) elements[i].GetComponentInChildren<MeshRenderer>().material.color = tmpColor;
+        if(i>=0 && i < elements.Count) elements[i].GetComponent<Element>().SetToDefaultColor();
 
         i = elements.Count - 1;
         for (int j = 0; j < elements.Count; j++)
@@ -52,8 +52,7 @@ public class TutorialSteps : MonoBehaviour
             elements[j].SetActive(true);
         }
         //Задаем следующий элемент зеленым
-        tmpColor = elements[i].GetComponentInChildren<MeshRenderer>().material.color;
-        elements[i].GetComponentInChildren<MeshRenderer>().material.color = new Color(0, 1, 0, 0.5f);
+        elements[i].GetComponent<Element>().SetToGreenColor();
         elements[i].SetActive(true);
 
         heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
@@ -74,7 +73,7 @@ public class TutorialSteps : MonoBehaviour
             if (i <= elements.Count - 1)
             {
                 //сделать текущий цветом из буфера
-                elements[i].GetComponentInChildren<MeshRenderer>().material.color = tmpColor;
+                elements[i].GetComponent<Element>().SetToDefaultColor();
 
                 elements[i].SetActive(true);
             } 
@@ -90,8 +89,7 @@ public class TutorialSteps : MonoBehaviour
                 heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
                 instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
                 //зеленым следующий элемент
-                tmpColor = elements[i].GetComponentInChildren<MeshRenderer>().material.color;
-                elements[i].GetComponentInChildren<MeshRenderer>().material.color = new Color(0, 1, 0, 0.5f);
+                elements[i].GetComponent<Element>().SetToGreenColor();
                 elements[i].SetActive(true);
             }
         }
@@ -100,7 +98,7 @@ public class TutorialSteps : MonoBehaviour
             if (i>=0)
             {
                 //сделать текущий цветом из буфера
-                elements[i].GetComponentInChildren<MeshRenderer>().material.color = tmpColor;
+                elements[i].GetComponent<Element>().SetToDefaultColor();
 
                 elements[i].SetActive(false);
             }
@@ -116,8 +114,7 @@ public class TutorialSteps : MonoBehaviour
                 heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
                 instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
                 //зеленым следующий элемент
-                tmpColor = elements[i].GetComponentInChildren<MeshRenderer>().material.color;
-                elements[i].GetComponentInChildren<MeshRenderer>().material.color = new Color(0, 1, 0, 0.5f);
+                elements[i].GetComponent<Element>().SetToGreenColor();
                 elements[i].SetActive(true);
             }
 
@@ -132,17 +129,15 @@ public class TutorialSteps : MonoBehaviour
                 //следующий, зеленый элемент, выключить и сделать цветом из буфера
                 if (i <= elements.Count - 1)
                 {
-                    elements[i].GetComponentInChildren<MeshRenderer>().material.color = tmpColor;
+                    elements[i].GetComponent<Element>().SetToDefaultColor();
                     elements[i].SetActive(false);
                 }
 
                 i--;
-                //elements[i].SetActive(false);
                 heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
                 instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
                 //текущий становится зеленым и не выключается
-                tmpColor = elements[i].GetComponentInChildren<MeshRenderer>().material.color;
-                elements[i].GetComponentInChildren<MeshRenderer>().material.color = new Color(0, 1, 0, 0.5f);
+                elements[i].GetComponent<Element>().SetToGreenColor();
             }
         }
         else
@@ -153,7 +148,7 @@ public class TutorialSteps : MonoBehaviour
                 //предыдущий, зеленый элемент, включить и сделать цветом из буфера
                 if (i >= 0)
                 {
-                    elements[i].GetComponentInChildren<MeshRenderer>().material.color = tmpColor;
+                    elements[i].GetComponent<Element>().SetToDefaultColor();
                     elements[i].SetActive(true);
                 }
 
@@ -162,8 +157,7 @@ public class TutorialSteps : MonoBehaviour
                 heading.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Heading;
                 instruction.GetComponent<TMP_Text>().text = elements[i].GetComponent<Element>().Instruction;
                 //текущий становится зеленым и не выключается
-                tmpColor = elements[i].GetComponentInChildren<MeshRenderer>().material.color;
-                elements[i].GetComponentInChildren<MeshRenderer>().material.color = new Color(0, 1, 0, 0.5f);
+                elements[i].GetComponent<Element>().SetToGreenColor();
             }
         }
     }
