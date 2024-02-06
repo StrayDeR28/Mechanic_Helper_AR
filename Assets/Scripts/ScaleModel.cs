903e56cd8ef0d9c7f9911d8e5494e61671040b0a
Фёдор <fedor.pavlov.02@mail.ru>
2024-02-06 18:06:20 +0700

    Switched to image tracking + obj track scene
    Много мусора, нужно удалить лишние спрайты, добавить в таргет модельку для отслеживания

--- Assets/Scripts/ScaleModel.cs
+++ Assets/Scripts/ScaleModel.cs
@@ -7,6 +7,7 @@ public class ScaleModel : MonoBehaviour
 {
     [SerializeField] private GameObject model;
     [SerializeField] private float scaleCoef = 1.5f;
+    private bool isRotated = false;
  
     public void ScaleUp()
     {
@@ -21,5 +22,19 @@ public class ScaleModel : MonoBehaviour
         Vector3 position = model.transform.localPosition;
         model.transform.localScale = new Vector3(scale.x / scaleCoef, scale.y / scaleCoef, scale.z / scaleCoef);
         //model.transform.localPosition = new Vector3(position.x / scaleCoef, position.y / scaleCoef, position.z / scaleCoef);
+    }
+    public void Rotate()
+    {
+        Quaternion rotation = model.transform.localRotation;
+        if (!isRotated)
+        {
+            model.transform.localRotation = new Quaternion(rotation.x + 90, rotation.y, rotation.z, rotation.w);
+            isRotated = true;
+        }
+        else
+        {
+            model.transform.localRotation = new Quaternion(rotation.x - 90, rotation.y, rotation.z, rotation.w);
+            isRotated = false;
+        }
     }
 }
